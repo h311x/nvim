0beedd91be0ee36bfc6c082476101ffe4f154920
Helix <amirtadzhiev@gmail.com>
2024-05-13 08:33:30 +0300

    use bash on windows

--- init.lua
+++ init.lua
@@ -1,2 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+if vim.loop.os_uname().sysname == "Windows_NT" then
+  vim.o.shell = "C:/Program Files/Git/bin/bash.exe"
+  vim.o.shellcmdflag = "-s"
+end
